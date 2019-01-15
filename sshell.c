52554dee394a0885961426ca6c2e4d997c7e9110
jackied <jackied@ad3.ucdavis.edu@pc14.cs.ucdavis.edu>
2019-01-14 19:03:25 -0800

    Phase 3 80% except more than 1 flag check

--- sshell.c
+++ sshell.c
@@ -12,17 +12,52 @@ int main(int argc, char *argv[])
 		// Display prompt
 		printf("sshell$ ");
 		// Read line from input
-		char cmd[512];
-		fgets(cmd, 512, stdin);
+		// https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
+		char userInput[512];
+		fgets(userInput, 512, stdin);
 		char *pos;
-		if ((pos = strchr(cmd, '\n')) != NULL) {
+		if ((pos = strchr(userInput, '\n')) != NULL) {
 			*pos = '\0';
 		}
 		else {
 			perror("something goes wrong");
 		}
+
 		// Prase line
-			// Phase 4
+		char userInputCopy[512];
+		strcpy(userInputCopy, userInput);
+		int strLength = strlen(userInput);
+		printf("strLength is: %d\n", strLength);
+		int index = 0;
+		int wordIndex = 0;
+		char *target[16];
+
+		// Remove all white space and tab to '\0'
+		while(index < strLength) {
+			if (userInput[index] == ' ' || userInput[index] == '\t') {
+				userInput[index] = '\0';
+			}
+			index++;
+		}
+
+		index = 0;
+		while(index < strLength) {
+			while (userInput[index] == '\0' && index < strLength) {
+				index++;
+			}
+			if (index >= strLength) {
+				break;
+			}
+			target[wordIndex] = userInput + index;
+			wordIndex++;
+			while (userInput[index] != '\0' && index < strLength) {
+				index++;
+			}
+		}
+
+
+		target[wordIndex] = NULL;
+
 
 		// Fork to handle test
 		pid_t pid;
@@ -35,13 +70,13 @@ int main(int argc, char *argv[])
 			// char *test[2] = {"date", NULL};
 			// execvp(test[0], test);
 			// perror("execvp");
-			char *fileArgPtr[2] = {cmd, NULL};
-			execvp(fileArgPtr[0], fileArgPtr);
+			// char *fileArgPtr[2] = {userInput, NULL};
+			execvp(target[0], target);
 			perror("execvp");
 		} else if (pid > 0) {
 			// parent
 			waitpid(-1, &status, 0);
-			fprintf(stderr, "+ completed '%s' [%d]\n", cmd, WEXITSTATUS(status));
+			fprintf(stderr, "+ completed '%s' [%d]\n", userInputCopy, WEXITSTATUS(status));
 		} else {
 			perror("fork");
 			exit(-1);
